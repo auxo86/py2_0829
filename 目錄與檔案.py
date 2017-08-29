2b5cf05ef7b5f5210d745da55c0b3f43cf5bf760
auxo <auxo86@gmail.com>
2017-08-29 18:38:58 +0800

    加入執行後的註解

--- 目錄與檔案.py
+++ 目錄與檔案.py
@@ -10,17 +10,17 @@ print os.getcwd()
 # 印出本身的檔案，然後取得後面加入的所有參數
 # 第0個參數是檔案本身
 # 命令列的參數從第一個開始
-
-# D:\PycharmProjects\py2_0829>python 目錄與檔案.py -a -b -c a=b
-# D:\PycharmProjects\py2_0829
-# D:\PycharmProjects\py2_0829\foo
-# 目錄與檔案.py
-# -a
-# -b
-# -c
-# a=b
-# D:\Python27\python.exe
-
+'''
+D:\PycharmProjects\py2_0829>python 目錄與檔案.py -a -b -c a=b
+D:\PycharmProjects\py2_0829
+D:\PycharmProjects\py2_0829\foo
+目錄與檔案.py
+-a
+-b
+-c
+a=b
+D:\Python27\python.exe
+'''
 for item in sys.argv:
     print item
 
